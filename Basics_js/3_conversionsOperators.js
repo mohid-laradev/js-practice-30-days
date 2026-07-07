44d3077cf97b2cf4466fcbd987041d400e7a6524
mohid-laradev <mohid1099@gmail.com>
2026-07-08 01:51:20 +0500

    practice the datatypes conversitions

---
+++ Basics_js/3_conversionsOperators.js
@@ -0,0 +1,26 @@
+
+let score = "33";
+
+console.log(score); // 33
+console.log(typeof score); // string
+
+// Convert string to number
+score = Number(score);
+
+console.log(score); // 33
+console.log(typeof score); // number
+
+
+console.log(typeof NaN); // number
+
+let result = Number("hello");
+console.log(result);
+console.log(typeof result);
+
+let no = 90;
+
+let str = String(no);
+console.log(no);
+console.log(typeof no);
+console.log(no.toString());
+console.log(typeof no.toString());
