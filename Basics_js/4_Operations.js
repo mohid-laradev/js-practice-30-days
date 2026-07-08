08d4da131bf9e5f2062721c039f79f4c025903f9
mohid-laradev <mohid1099@gmail.com>
2026-07-08 17:53:01 +0500

    comparison and operations

---
+++ Basics_js/4_Operations.js
@@ -0,0 +1,34 @@
+
+// ******************************Operations******************************
+
+// Arithmetic Operations
+// 1. + (addition)
+// 2. - (subtraction)
+// 3. * (multiplication)
+// 4. / (division)
+// 5. % (modulus)
+
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// Add string and number
+let str1= "Hello";
+let num1 = 5;
+console.log(str1 + num1);  
+
+// 
+console.log("1" + 1); // 11
+
+let x = "2" + "5";
+
+// let no = Number(x);
+
+console.log(x);
+
+ 
---
+++ Basics_js/5_comparison.js
@@ -0,0 +1,27 @@
+
+// Comparison Operators
+
+// Equal to (==)
+console.log(5 == '5'); // true
+
+// Not equal to (!=)
+console.log(5 != '5'); // false
+
+// Strict equal to (===)
+console.log(5 === '5'); // false
+
+// Strict not equal to (!==)
+console.log(5 !== '5'); // true 
+
+// Greater than (>)
+console.log(5 > 3); // true
+
+// Less than (<)    
+console.log(5 < 3); // false
+
+// Greater than or equal to (>=)            
+console.log(5 >= 5); // true
+
+// Less than or equal to (<=)            
+console.log(5 <= 3); // false
+
